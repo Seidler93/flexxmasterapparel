1e02fb2e9dd4417ca2e7c3724b790dd6273c7a4b
Alex Seidler <ajseidler0526@gmail.com>
2025-05-23 02:09:34 -0500

    updated server url

--- client/src/components/ConfirmModal.jsx
+++ client/src/components/ConfirmModal.jsx
@@ -35,7 +35,7 @@ export default function ConfirmModal({ isOpen, onClose, cart, onConfirm, locatio
 
 
     try {
-      const response = await fetch('https://flexxmasterapparel.onrender.com', {
+      const response = await fetch('https://flexxmasterapparel.onrender.com/submit-order', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(orderData)
